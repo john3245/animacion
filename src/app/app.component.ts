24c6ee97356d88ccbe51aa31477df15871dcb4d7
john3245 <52746923+john3245@users.noreply.github.com>
2020-03-02 20:09:16 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {trigger,state,style,animate,keyframes,transition} from '@angular/animations'
+
+declare var $:any;
+declare var Jquery:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations : [
+    trigger('box',[
+        state('inactive',style({
+            backgroundColor:'#ABEBC6',
+            transform:'scale(2) translateX(40px)'
+        })),
+        state('active',style({
+            backgroundColor:'#D68910',
+            transform:'scale(3) translateX(40px)'
+        })),
+        transition('inactive => active',animate('500ms ease-in')),
+        transition('active => inactive',animate('1000ms ease-out'))
+    ])
+]
 })
 export class AppComponent {
   title = 'example';
+
+  public state:string="";
+  constructor(){}
+  toggleState()
+  {
+    this.state=this.state==='active' ? 'inactive' : 'active';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {trigger,state,style,animate,keyframes,transition} from '@angular/animations'
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
